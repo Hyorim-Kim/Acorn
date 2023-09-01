39ba765823d037b6c5f81a5f5a7c1300ee8c5cea
HK <hyorim.kim@students.mq.edu.au>
2023-09-01 18:26:41 +0900

    MyBatis annotation

---
+++ java_web11member_anno/src/main/java/pack/business/DataFormbean.java
@@ -0,0 +1,11 @@
+package pack.business;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DataFormbean {
+	private String id,name,passwd;
+	private String colname;
+}
